9545437e3da3081789a10db0be5bb4a547a1b9e8
agawas <agawas@iu.edu>
2024-06-17 20:39:06 -0400

    Added multiple beans and records

--- src/main/java/com/example/learn_spring_framework/App02HelloWorldSpring.java
+++ src/main/java/com/example/learn_spring_framework/App02HelloWorldSpring.java
@@ -12,6 +12,10 @@ public class App02HelloWorldSpring {
 	public static void main(String[] args) {
 		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("person2"));
+		System.out.println(context.getBean("address"));
 		
 
 	}
--- src/main/java/com/example/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/example/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,11 +3,34 @@ package com.example.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {}
+record Address(String street, String city) {}
+
 @Configuration
 public class HelloWorldConfiguration {
 	
 	@Bean
 	public String name() {
 		return "Akshata";
+	}
+	
+	@Bean
+	public int age() {
+		return 23;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Ravi", 26);
+	}
+	
+	@Bean
+	public Person person2() {
+		return new Person("Apeksha", 22);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("7th Street", "Bloomington");
 	}
 }
