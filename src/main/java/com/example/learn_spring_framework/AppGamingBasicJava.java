54abdcb0d9d2f73530d57085c915184ff5f26420
agawas <agawas@iu.edu>
2024-06-17 20:16:02 -0400

    normal loose coupling example

---
+++ src/main/java/com/example/learn_spring_framework/AppGamingBasicJava.java
@@ -0,0 +1,19 @@
+package com.example.learn_spring_framework;
+
+import com.example.learn_spring_framework.game.GameRunner;
+import com.example.learn_spring_framework.game.MarioGame;
+import com.example.learn_spring_framework.game.PacManGame;
+import com.example.learn_spring_framework.game.SuperContraGame;
+
+public class AppGamingBasicJava {
+
+	public static void main(String[] args) {
+//		var game = new MarioGame();
+//		var game = new SuperContraGame();
+		var game = new PacManGame();
+		var gameRunner = new GameRunner(game);
+		gameRunner.run();
+
+	}
+
+}
