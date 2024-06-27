9ab52f81ef43fccb41f372866b0d0e2a09a44dc1
agawas <agawas@iu.edu>
2024-06-27 17:29:45 -0400

    JPA and EntityManager

--- src/main/java/com/springboot/learn_jpa_hibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/springboot/learn_jpa_hibernate/course/CourseCommandLineRunner.java
@@ -1,16 +1,19 @@
-package com.springboot.learn_jpa_hibernate.course.jdbc;
+package com.springboot.learn_jpa_hibernate.course;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.springboot.learn_jpa_hibernate.course.Course;
+import com.springboot.learn_jpa_hibernate.course.jpa.CourseJpaRepository;
 
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner{
+public class CourseCommandLineRunner implements CommandLineRunner{
+	
+//	@Autowired
+//	private CourseJdbcRepository repository;
 	
 	@Autowired
-	private CourseJdbcRepository repository;
+	private CourseJpaRepository repository;
 	
 	@Override
 	public void run(String... args) throws Exception {
