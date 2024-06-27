ce3ebaa900c09d8691f57772e27da4101e055e31
agawas <agawas@iu.edu>
2024-06-27 15:52:33 -0400

    Spring Boot Application

---
+++ src/main/java/com/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,22 @@
+package com.springboot.learn_spring_boot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	
+	
+	@RequestMapping("/courses")
+	public List<Course> retrieveAllCourses(){
+		return Arrays.asList(
+				new Course(1, "Learn AWS", "James Shanon"),
+				new Course(2, "Learn DevOps", "Arnold Baker"),
+				new Course(3, "Learn AI", "Andrew Ng")
+				
+				);
+	}
+}
