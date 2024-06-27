be18d42eba3ac8ab6d050cd11b5483eebe09ac45
agawas <agawas@iu.edu>
2024-06-27 17:44:14 -0400

    Spring Data JPA

--- src/main/java/com/springboot/learn_jpa_hibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/springboot/learn_jpa_hibernate/course/CourseCommandLineRunner.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.springboot.learn_jpa_hibernate.course.jpa.CourseJpaRepository;
+import com.springboot.learn_jpa_hibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner{
@@ -12,22 +12,47 @@ public class CourseCommandLineRunner implements CommandLineRunner{
 //	@Autowired
 //	private CourseJdbcRepository repository;
 	
+//	@Autowired
+//	private CourseJpaRepository repository;
+	
 	@Autowired
-	private CourseJpaRepository repository;
+	private CourseSpringDataJpaRepository repository;
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1, "Learn DevOps", "Baker Keith"));
-		repository.insert(new Course(2, "Learn AWS", "Billy Grande"));
-		repository.insert(new Course(3, "Learn AI", "Matthew Francisco"));
-		repository.insert(new Course(4, "Learn React", "Anurag Bisht"));
-		repository.insert(new Course(5, "Learn ML", "Keith Simmons"));
-		repository.deleteById(3);
+//		repository.insert(new Course(1, "Learn DevOps", "Baker Keith"));
+//		repository.insert(new Course(2, "Learn AWS", "Billy Grande"));
+//		repository.insert(new Course(3, "Learn AI", "Matthew Francisco"));
+//		repository.insert(new Course(4, "Learn React", "Anurag Bisht"));
+//		repository.insert(new Course(5, "Learn ML", "Keith Simmons"));
 		
 		
-		System.out.println(repository.findById(2));
-		System.out.println(repository.findById(4));
-		System.out.println(repository.findById(5));
+		
+		
+		
+		repository.save(new Course(1, "Learn DevOps", "Baker Keith"));
+		repository.save(new Course(2, "Learn AWS", "Billy Grande"));
+		repository.save(new Course(3, "Learn AI", "Matthew Francisco"));
+		repository.save(new Course(4, "Learn React", "Anurag Bisht"));
+		repository.save(new Course(5, "Learn ML", "Keith Simmons"));
+		
+		
+		
+		
+		repository.deleteById(3l);
+		
+		
+		System.out.println(repository.findById(2l));
+		System.out.println(repository.findById(4l));
+		System.out.println(repository.findById(5l));
+		
+		
+		System.out.println(repository.findByAuthor("Anurag Bisht"));
+		System.out.println(repository.findByName("Learn AWS"));
+		
+		
+		
+		
 	}
 
 }
